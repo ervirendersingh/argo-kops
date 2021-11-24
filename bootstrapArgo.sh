01676113437b41827150091559709516e6026295
Virender Singh <virendersingh@192-168-1-102.tpgi.com.au>
2021-11-24 20:59:16 +1100

    added guestbook and other services

--- bootstrapArgo.sh
+++ bootstrapArgo.sh
@@ -8,11 +8,5 @@ export awsRegion=us-east-2a #aws region where cluster will reside
 
 # set the cluster to be used with kubectl
 kubectl config set-cluster $KOPS_CLUSTER_NAME --insecure-skip-tls-verify
-kubectl create namespace argocd
-kubectl apply -f argocd/argocd/argocd.yml -n argocd
 
-kubectl apply -f argocd/argo-projects/project-argocd.yml -n argocd
-kubectl apply -f argocd/argo-projects/project-deployments.yml -n argocd
-
-kubectl apply -f argocd/argo-bootstrap-apps/argocd.yml -n argocd
-kubectl apply -f argocd/argo-bootstrap-apps/deployments.yml -n argocd
+kustomize build argocd-namespace | kubectl apply -f .
